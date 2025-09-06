448f6ec0cf239762a95b4540f1698bca675d4226
st2.72panov <st2.72panov@gmail.com>
2025-09-06 21:44:20 +0300

    init

---
+++ task1.cu
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <chrono>
+#include <cuda_runtime.h>
+
+void fill_array(float* A, const int& N) {
+    for (int i = 0; i < N; ++i)
+        A[i] = 1.0f / std::rand();
+}
+
+///////////////////////////////////////////////////////////
+// CPU
+
+void scaleVectorCPU(const float* A, float* B, const int N, const float k) {
+    for (int i = 0; i < N; ++i)
+        B[i] = A[i] * k;
+}
+
+void test_cpu(const int N, const float k) {
+    float* A = (float*) malloc(N * sizeof(float));
+    float* B = (float*) malloc(N * sizeof(float));
+    fill_array(A, N);
+
+    auto start = std::chrono::high_resolution_clock::now();
+    scaleVectorCPU(A, B, N, k);
+    auto end = std::chrono::high_resolution_clock::now();
+
+    free(A);
+    free(B);
+
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+    std::cout << "CPU: " << duration.count() / 1000.0 << "ms" << std::endl;
+}
+
+///////////////////////////////////////////////////////////
+// GPU
+
+__global__ void scaleVectorCUDA(const float* A, float* B, int N, float k) {
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    if (idx < N)
+        B[idx] = A[idx] * k;
+}
+
+void test_GPU(const int N, const float k) {
+    const auto THREADS_PER_BLOCK = 32;
+
+    float* h_A = (float*) malloc(N * sizeof(float));
+    float* h_B = (float*) malloc(N * sizeof(float));
+    fill_array(h_A, N);
+    
+    float duration;
+    float *d_A, *d_B;
+    cudaMalloc(&d_A, N * sizeof(float));
+    cudaMalloc(&d_B, N * sizeof(float));
+    cudaMemcpy(d_A, h_A, N * sizeof(float), cudaMemcpyHostToDevice);
+
+    cudaEvent_t start_e, end_e;
+    cudaEventCreate(&start_e);
+    cudaEventCreate(&end_e);
+    cudaEventRecord(start_e);
+    scaleVectorCUDA<<<std::ceil(1.0 * N / THREADS_PER_BLOCK), THREADS_PER_BLOCK>>>(d_A, d_B, N, k);
+    cudaEventRecord(end_e);
+    cudaEventSynchronize(end_e);
+    cudaEventElapsedTime(&duration, start_e, end_e);
+    cudaEventDestroy(start_e);
+    cudaEventDestroy(end_e);
+
+    cudaMemcpy(h_B, d_B, N * sizeof(float), cudaMemcpyDeviceToHost);
+    cudaFree(d_A);
+    cudaFree(d_B);
+    free(h_A);
+    free(h_B);
+
+    std::cout << "GPU: " << duration << "ms" << std::endl;
+}
+
+///////////////////////////////////////////////////////////
+
+int main() {
+    const int N = 1 * 1000 * 1000;
+    const float k = 2.5f;
+    test_cpu(N, k);
+    test_GPU(N, k);
+}
---
+++ task2.cu
@@ -0,0 +1,108 @@
+#include <iostream>
+#include <chrono>
+#include <cuda_runtime.h>
+
+void fill_array(unsigned char* A, const int& N) {
+    for (int i = 0; i < N; ++i)
+        A[i] = std::rand() % 256;
+}
+
+///////////////////////////////////////////////////////////
+// CPU
+
+void thresholdFilterCPU(
+    unsigned char* input,
+    unsigned char* output,
+    const int width,
+    const int height,
+    unsigned char threshold
+) {
+    const int size = width * height;
+    for (int i = 0; i < size; ++i)
+        output[i] = (input[i] > threshold) ? 1 : 0;
+}
+
+void test_cpu(const int width, const int height, unsigned char threshold) {
+    const int size = width * height;
+    const int size_bytes = size * sizeof(unsigned char);
+
+    unsigned char* A = (unsigned char*) malloc(size_bytes);
+    unsigned char* B = (unsigned char*) malloc(size_bytes);
+    fill_array(A, size);
+
+    auto start = std::chrono::high_resolution_clock::now();
+    thresholdFilterCPU(A, B, width, height, threshold);
+    auto end = std::chrono::high_resolution_clock::now();
+
+    free(A);
+    free(B);
+
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+    std::cout << "CPU: " << duration.count() / 1000.0 << "ms" << std::endl;
+}
+
+///////////////////////////////////////////////////////////
+// GPU
+
+__global__ void thresholdFilterCUDA(
+    unsigned char* input,
+    unsigned char* output, 
+    const int width,
+    const int height,
+    unsigned char threshold
+) {
+    const int x = blockDim.x * blockIdx.x + threadIdx.x;
+    const int y = blockDim.y * blockIdx.y + threadIdx.y;
+    if (x < width && y < height) {
+        const int idx = x + y * width;
+        output[idx] = (input[idx] > threshold) ? 1 : 0;
+    }
+}
+
+void test_GPU(const int width, const int height, unsigned char threshold) {
+    const auto BLOCK_WIDTH = 8;  // 8 * 8 = 64 threads per block
+    const int size = width * height;
+    const int size_bytes = size * sizeof(unsigned char);
+
+    unsigned char* h_A = (unsigned char*) malloc(size_bytes);
+    unsigned char* h_B = (unsigned char*) malloc(size_bytes);
+    fill_array(h_A, size);
+
+    unsigned char *d_A, *d_B;
+    cudaMalloc(&d_A, size_bytes);
+    cudaMalloc(&d_B, size_bytes);
+    cudaMemcpy(d_A, h_A, size_bytes, cudaMemcpyHostToDevice);
+    dim3 blockSize(BLOCK_WIDTH, BLOCK_WIDTH);
+    dim3 gridSize(
+        (width + blockSize.x - 1) / blockSize.x,
+        (height + blockSize.y - 1) / blockSize.y);
+
+    float duration;
+    cudaEvent_t start_e, end_e;
+    cudaEventCreate(&start_e);
+    cudaEventCreate(&end_e);
+    cudaEventRecord(start_e);
+
+    thresholdFilterCUDA<<<gridSize, blockSize>>>(d_A, d_B, width, height, threshold);
+    
+    cudaEventRecord(end_e);
+    cudaEventSynchronize(end_e);
+    cudaEventElapsedTime(&duration, start_e, end_e);
+    cudaEventDestroy(start_e);
+    cudaEventDestroy(end_e);
+    std::cout << "GPU: " << duration << "ms" << std::endl;
+    
+    cudaMemcpy(h_B, d_B, size_bytes, cudaMemcpyDeviceToHost);
+    cudaFree(d_A);
+    cudaFree(d_B);
+    free(h_A);
+    free(h_B);
+}
+
+///////////////////////////////////////////////////////////
+
+int main() {
+    const int W = 1024, H = 1024, T = 128;
+    test_cpu(W, H, T);
+    test_GPU(W, H, T);
+}
